8bb63fb0ceb0f15f04d23ae81afd863d42ea67dd
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-06 17:48:16 +0100

    Video 16 done

---
+++ navbar/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="antialiased text-gray-900">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ navbar/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="bg-gray-900 flex justify-between items-center px-4 py-3">
+    <div class="flex items-center text-2xl text-bold">
+      <CubeTransparentIcon class="h-12 w-12 text-indigo-500" /> <span class="ml-2 text-white">Work<span class="text-indigo-500">cation</span></span>
+    </div>
+    <div>
+      <button type="button" class="block text-gray-500 focus:text-white focus:outline-none hover:text-white">
+        <MenuIcon class="h-6 w-6 fill-current" />
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+import { CubeTransparentIcon, MenuIcon } from '@vue-hero-icons/solid';
+
+export default {
+  components: {
+    CubeTransparentIcon,
+    MenuIcon,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
