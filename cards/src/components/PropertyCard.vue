7da01e84bcf19b8f0fb6bd2c612ee23637844b70
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-06 17:14:37 +0100

    Video 14 done

--- cards/src/components/PropertyCard.vue
+++ cards/src/components/PropertyCard.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-white border rounded-lg overflow-hidden">
-    <!-- for IE11
-      <div class="h-48 bg-cover bg-center" :style="{backgroundImage: `url(${property.imageUrl}`}"/>
-    -->
-    <img class="h-48 w-full object-cover" :src="property.imageUrl" :alt="property.imageAlt" />
+    <div class="relative bg-red-500 pb-2/3">
+      <!-- for IE11
+        <div class="absolute h-full w-full bg-cover bg-center" :style="{backgroundImage: `url(${property.imageUrl}`}"/>
+      -->
+      <img class="absolute h-full w-full object-cover" :src="property.imageUrl" :alt="property.imageAlt" />
+    </div>
     <div class="p-6">
       <div class="flex items-baseline">
         <span class="inline-block bg-teal-300 text-teal-700 font-semibold text-xs px-2 rounded-full uppercase">New</span>
