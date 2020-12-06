d8b8c8d587055ee8a1f3324df0000782340d9aa4
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-06 18:09:06 +0100

    Video 18 done

--- navbar/src/components/Navbar.vue
+++ navbar/src/components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
-  <header class="bg-gray-900">
-    <div class="flex justify-between items-center px-4 py-3">
+  <header class="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+    <div class="flex justify-between items-center px-4 py-3 sm:px-0 sm:py-0">
       <div class="flex items-center text-2xl text-bold">
         <CubeTransparentIcon class="h-12 w-12 text-indigo-500" />
         <span class="ml-2 text-white"
           >Work<span class="text-indigo-500">cation</span></span
         >
       </div>
-      <div>
+      <div class="sm:hidden">
         <button
           type="button"
-          class="block text-gray-500 focus:text-white focus:outline-none hover:text-white"
+          class="block text-gray-300 focus:text-white focus:outline-none hover:text-white"
           @click="isOpen = !isOpen"
         >
           <MenuIcon class="h-6 w-6 fill-current" v-if="!isOpen" />
@@ -18,7 +18,7 @@
         </button>
       </div>
     </div>
-    <div class="px-2 pt-2 pb-4" :class="isOpen ? 'block' : 'hidden'">
+    <div class="px-2 pt-2 pb-4 sm:flex sm:px-0 sm:pt-0 sm:pb-0" :class="isOpen ? 'block' : 'hidden'">
       <a
         href="#"
         class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
@@ -26,12 +26,12 @@
       >
       <a
         href="#"
-        class="block mt-1 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
+        class="block mt-1 sm:mt-0 sm:ml-2 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
         >Trips</a
       >
       <a
         href="#"
-        class="block mt-1 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
+        class="block mt-1 sm:mt-0 sm:ml-2 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
         >Messages</a
       >
     </div>
