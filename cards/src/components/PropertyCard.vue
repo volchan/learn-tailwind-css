dc16f9263a3b329d41e96bb092c2a7e494dd5df7
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-06 16:59:03 +0100

    Video 12 done

--- cards/src/components/PropertyCard.vue
+++ cards/src/components/PropertyCard.vue
@@ -2,10 +2,13 @@
   <div class="bg-white border rounded-lg overflow-hidden">
     <img :src="property.imageUrl" :alt="property.imageAlt" />
     <div class="p-6">
-      <div class="text-gray-600 text-xs uppercase font-semibold tracking-wide">
-        {{ property.beds }} beds &bull; {{ property.baths }} baths
+      <div class="flex items-baseline">
+        <span class="inline-block bg-teal-300 text-teal-700 font-semibold text-xs px-2 rounded-full uppercase">New</span>
+        <div class="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
+          {{ property.beds }} beds &bull; {{ property.baths }} baths
+        </div>
       </div>
-      <h4 class="font-semibold text-lg leading-tight truncate">
+      <h4 class="mt-1 font-semibold text-lg leading-tight truncate">
         {{ property.title }}
       </h4>
       <div class="mt-1">
@@ -13,7 +16,7 @@
         <span class="text-gray-600 text-sm">/ wk</span>
       </div>
       <div class="mt-2 flex items-center">
-        <StarIcon class="h-4 w-4" v-for="i in 5" :key="i" :class="i <= property.rating ? 'text-teal-500' : 'text-gray-400'" />
+        <StarIcon class="h-4 w-4" v-for="i in 5" :key="i" :class="i <= property.rating ? 'text-teal-600' : 'text-gray-400'" />
         <span class="ml-2 text-gray-600 text-sm">{{ property.reviewCount }} reviews</span>
       </div>
     </div>
