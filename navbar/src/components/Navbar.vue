4596d4c469a6dc90b9164cfca0be268cb95a333d
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-06 18:00:21 +0100

    Video 17 done

--- navbar/src/components/Navbar.vue
+++ navbar/src/components/Navbar.vue
@@ -1,26 +1,56 @@
 <template>
-  <header class="bg-gray-900 flex justify-between items-center px-4 py-3">
-    <div class="flex items-center text-2xl text-bold">
-      <CubeTransparentIcon class="h-12 w-12 text-indigo-500" /> <span class="ml-2 text-white">Work<span class="text-indigo-500">cation</span></span>
+  <header class="bg-gray-900">
+    <div class="flex justify-between items-center px-4 py-3">
+      <div class="flex items-center text-2xl text-bold">
+        <CubeTransparentIcon class="h-12 w-12 text-indigo-500" />
+        <span class="ml-2 text-white"
+          >Work<span class="text-indigo-500">cation</span></span
+        >
+      </div>
+      <div>
+        <button
+          type="button"
+          class="block text-gray-500 focus:text-white focus:outline-none hover:text-white"
+          @click="isOpen = !isOpen"
+        >
+          <MenuIcon class="h-6 w-6 fill-current" v-if="!isOpen" />
+          <XIcon class="h-6 w-6 fill-current" v-if="isOpen" />
+        </button>
+      </div>
     </div>
-    <div>
-      <button type="button" class="block text-gray-500 focus:text-white focus:outline-none hover:text-white">
-        <MenuIcon class="h-6 w-6 fill-current" />
-      </button>
+    <div class="px-2 pt-2 pb-4" :class="isOpen ? 'block' : 'hidden'">
+      <a
+        href="#"
+        class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
+        >List your property</a
+      >
+      <a
+        href="#"
+        class="block mt-1 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
+        >Trips</a
+      >
+      <a
+        href="#"
+        class="block mt-1 px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
+        >Messages</a
+      >
     </div>
   </header>
 </template>
 
 <script>
-import { CubeTransparentIcon, MenuIcon } from '@vue-hero-icons/solid';
+import { CubeTransparentIcon, MenuIcon, XIcon } from '@vue-hero-icons/solid';
 
 export default {
   components: {
     CubeTransparentIcon,
     MenuIcon,
+    XIcon,
   },
   data() {
-    return {};
+    return {
+      isOpen: false,
+    };
   },
 };
 </script>
